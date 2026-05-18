8cf5fede77c711aab88626697f3f9bf7273ccf02
Tej Kiran Yenugunti <tejkiranyenugunti@gmail.com>
2026-05-18 11:36:18 -0700

    Added LC3

---
+++ string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str):
+
+        seen = set()
+        left = 0
+        longest = 0
+
+        for right in range(len(s)):
+
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+
+            longest = max(longest, right - left + 1)
+
+        return longest
+
+
+obj = Solution()
+
+print(obj.lengthOfLongestSubstring("abcabcbb"))  
+print(obj.lengthOfLongestSubstring("bbbbb"))     
+print(obj.lengthOfLongestSubstring("pwwkew"))    
+
+# 3. Longest Substring Without Repeating Characters - LC3
+# I used set for storing the characters in the current window. I used two pointers, left and right, to represent the current window.    and after checking irt and  moving i updated accordingly with the length . 
