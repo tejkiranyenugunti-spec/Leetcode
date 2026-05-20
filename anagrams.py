e994f81f2ae1098a9f0f1be19ec6c1453074fe27
Tej Kiran Yenugunti <tejkiranyenugunti@gmail.com>
2026-05-19 17:02:34 -0700

    Add explanation for groupAnagrams function
    Added a comment explaining the logic behind grouping anagrams using a dictionary.

--- anagrams.py
+++ anagrams.py
@@ -21,3 +21,5 @@ obj = Solution()
 
 print(obj.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 
+#LC 49. Group Anagrams
+# I used dictionary, I grouped the words which have the same letters,  each word is sorted  and used it as a kjey in the dictionary, then I added the word to the list of the key,  if the sorted word is same  as another word they will be added to the same zone group, then i returned the values of the dictionary. 
