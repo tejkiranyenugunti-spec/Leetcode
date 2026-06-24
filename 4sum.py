bff3eb815680331805d1d93ed7d3c376e682423c
Tej Kiran Yenugunti <tejkiranyenugunti@gmail.com>
2026-06-24 14:43:22 -0700

    4sum

---
+++ 4sum.py
@@ -0,0 +1,40 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        ans = []
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left, right = j + 1, n - 1
+
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return ans
+
+print(Solution().fourSum([1, 0, -1, 0, -2, 2], 0))
+print(Solution().fourSum([2, 2, 2, 2, 2], 8))
+
+# I used a two-pointer approach . First, I sorted the input list and then used two nested loops to fix the first two numbers. For the remaining two numbers, I used a left and right pointer to find pairs that sum up to the target. I also included checks to skip duplicate values to ensuren the result.
